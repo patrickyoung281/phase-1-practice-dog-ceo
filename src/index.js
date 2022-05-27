e60548e12a6f9645cea2161c3eca8918067500ca
Patrick Young <patrickyoung281@gmail.com>
2022-05-27 16:26:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 console.log('%c HI', 'color: firebrick')
 
+let allBreeds;
 
 fetch("https://dog.ceo/api/breeds/image/random/4") 
     .then((resp) => resp.json())
-    .then((json) => console.log(json))
     .then(function (data) {
         renderDogs(data)
     })
@@ -11,13 +11,52 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
 
 function renderDogs (imageArray) {
     const addImages = document.getElementById("dog-image-container")
-    
-    imageArray.forEach(element => {
-        const h1 = document.createElement("h1")
-        h1.appendChild("dog-image-container")
-        h1.innerHTML = element
-        addImages.appendChild(h1)
+    console.log(imageArray)
+    imageArray.message.forEach(element => {
+        const img = document.createElement("img")
+        img.src = element
+        addImages.appendChild(img) 
     });
         
     }
     
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then((resp) => resp.json())
+        .then(function(data) {
+            allBreeds = Object.keys(data.message)
+            addDogBreeds(allBreeds)
+        })
+        
+function addDogBreeds (breedList) {
+    const breeds = document.getElementById("dog-breeds")
+    breedList.forEach(breed => {
+        const createLi = document.createElement("li")
+        createLi.innerText = breed
+        createLi.addEventListener("click", changeColor)
+        breeds.appendChild(createLi)
+    })
+}
+
+function changeColor (e) {
+    console.log("event", e.target)
+    let li = e.target
+    li.style.color = "red"
+}
+
+const dropDown = document.getElementById("breed-dropdown")
+
+dropDown.addEventListener("change", firstLetterSelector) 
+
+
+function firstLetterSelector (e) {
+    const userChoice = e.target.value
+    const firstLetters = allBreeds.filter((breed) => {
+        console.log("breed", breed[0], userChoice)
+    
+        return (userChoice === breed[0])
+    
+}) 
+addDogBreeds(firstLetters)
+console.log("First Letter", firstLetters)
+    }
+
